73a4d8afb7ba788adc192fdbf4465bc4a626def6
Nathan Vu <56103559+mrnathanvu@users.noreply.github.com>
2021-11-12 02:09:05 -0800

    Finish the project

--- samples/sendfile/sendfilecli.py
+++ samples/sendfile/sendfilecli.py
@@ -1,17 +1,11 @@
-
 # *******************************************************************
 # This file illustrates how to send a file using an
 # application-level protocol where the first 10 bytes
 # of the message from client to server contain the file
 # size and the rest contain the file data.
 # *******************************************************************
-import socket
-import os
-import sys
 
-# Command line checks 
-if len(sys.argv) < 2:
-	print("USAGE python " + sys.argv[0] + " <FILE NAME>")
+import socket
 
 # Server address
 serverAddr = socket.gethostbyname(socket.gethostname())
@@ -19,12 +13,6 @@ print("Client is listening on: ", serverAddr)
 
 # Server port
 serverPort = 5050
-
-# The name of the file
-fileName = sys.argv[1]
-
-# Open the file
-fileObj = open(fileName, "r")
 
 # Create a TCP socket
 connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,48 +26,171 @@ numSent = 0
 # The file data
 fileData = None
 
+PUT_CMD = 'put'
+GET_CMD = 'get'
+QUIT_CMD = 'quit'
+LIST_CMD = 'ls'
+FORMAT = 'utf-8'
+
+
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+
+def recvAll(sock, numBytes):
+    # The buffer
+    recvBuff = ""
+
+    # The temporary buffer
+    tmpBuff = ""
+
+    # Keep receiving till all is received
+    while len(recvBuff) < numBytes:
+
+        # Attempt to receive bytes
+        tmpBuff = sock.recv(numBytes).decode(FORMAT)
+
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
+
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff
+
+    return recvBuff
+
+
 # Keep sending until all is sent
 while True:
-	
-	# Read 65536 bytes of data
-	fileData = fileObj.read(65536)
-	
-	# Make sure we did not hit EOF
-	if fileData:
-			
-		# Get the size of the data read
-		# and convert it to string
-		dataSizeStr = str(len(fileData))
 
-		# Prepend 0's to the size string
-		# until the size is 10 bytes
-		while len(dataSizeStr) < 10:
-			dataSizeStr = "0" + dataSizeStr
+    user_input = input('fpt> ')
 
-		# dataSizeStr += b' ' * (10 - len(dataSizeStr))
+    # server_input = connSock.recv(1024).decode(FORMAT)
+    # print("Received server input: ", server_input)
 
-		# Prepend the size of the data to the
-		# file data.
-		fileData = dataSizeStr + fileData
-		
-		# The number of bytes sent
-		numSent = 0
-		
-		# Send the data!
-		while len(fileData) > numSent:
-			numSent += connSock.send(fileData[numSent:].encode('utf-8'))
-	
-	# The file has been read. We are done
-	else:
-		break
+    if user_input.find(PUT_CMD) != -1:
 
+        # The name of the file
+        fileName = user_input.replace(PUT_CMD, '').strip()
+        print("--> fileName: ", fileName)
 
-print("Sent ", numSent, " bytes.")
-print(connSock.recv(2048).decode('utf-8'))
-	
+        connSock.send(user_input.encode(FORMAT))
+
+        # Open the file
+        fileObj = open(fileName, "r")
+
+        # Read 65536 bytes of data
+        fileData = fileObj.read(65536)
+
+        # Make sure we did not hit EOF
+        if fileData:
+
+            # Get the size of the data read
+            # and convert it to string
+            dataSizeStr = str(len(fileData))
+
+            # Prepend 0's to the size string
+            # until the size is 10 bytes
+            while len(dataSizeStr) < 10:
+                dataSizeStr = "0" + dataSizeStr
+
+            # Prepend the size of the data to the
+            # file data.
+            fileData = dataSizeStr + fileData
+
+            # The number of bytes sent
+            numSent = 0
+
+            # Send the data!
+            while len(fileData) > numSent:
+                numSent += connSock.send(fileData[numSent:].encode(FORMAT))
+
+        # The file has been read. We are done
+        else:
+            break
+
+        print("Client done sending file")
+        fileObj.close()
+        print("Sent ", numSent, " bytes.")
+
+    elif user_input.find(GET_CMD) != -1:
+
+        connSock.send(user_input.encode(FORMAT))
+
+        # The buffer to all data received from the
+        # the client.
+        fileData = ""
+
+        # The temporary buffer to store the received
+        # data.
+        recvBuff = ""
+
+        # The size of the incoming file
+        fileSize = 0
+
+        # The buffer containing the file size
+        fileSizeBuff = ""
+
+        # Receive the first 10 bytes indicating the
+        # size of the file
+        fileSizeBuff = recvAll(connSock, 10)
+
+        if fileSizeBuff:
+            # Get the file size
+            fileSize = int(fileSizeBuff)
+
+            print("The file size is: ", fileSize)
+
+            # Get the file data
+            fileData = recvAll(connSock, fileSize)
+
+            print("The file data is:\n", '"', fileData, '"\n')
+
+    elif user_input.find(LIST_CMD) != -1:
+
+        connSock.send(user_input.encode(FORMAT))
+
+        # The buffer to all data received from the
+        # the client.
+        fileData = ""
+
+        # The temporary buffer to store the received
+        # data.
+        recvBuff = ""
+
+        # The size of the incoming file
+        fileSize = 0
+
+        # The buffer containing the file size
+        fileSizeBuff = ""
+
+        # Receive the first 10 bytes indicating the
+        # size of the file
+        fileSizeBuff = recvAll(connSock, 10)
+
+        if fileSizeBuff:
+            # Get the file size
+            fileSize = int(fileSizeBuff)
+
+            print("The file size is: ", fileSize)
+
+            # Get the file data
+            fileData = recvAll(connSock, fileSize)
+
+            print("The file data is:\n", '"', fileData, '"\n')
+
+    elif user_input.find(QUIT_CMD) != -1:
+
+        connSock.send(user_input.encode(FORMAT))
+        break
+
+    else:
+        break
+
 # Close the socket and the file
+print("Disconnecting connections from Client")
 connSock.close()
-fileObj.close()
-	
-
-
--- samples/sendfile/sendfileserv.py
+++ samples/sendfile/sendfileserv.py
@@ -1,4 +1,3 @@
-
 # *****************************************************
 # This file implements a server for receiving the file
 # sent using sendfile(). The server receives a file and
@@ -6,16 +5,15 @@
 # *****************************************************
 
 import socket
-
-# The port on which to listen
-listenPort = 5050
+import subprocess
+import os
 
 # The IPv4 on which to listen
 listenServer = socket.gethostbyname(socket.gethostname())
 print("Server is listening on: ", listenServer)
 
-GET_MESSAGE = 'get'
-DISCONNECT_MESSAGE = 'quit'
+# The port on which to listen
+listenPort = 5050
 
 # Create a welcome socket. 
 welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,7 +22,17 @@ welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 welcomeSock.bind((listenServer, listenPort))
 
 # Start listening on the socket
-welcomeSock.listen()
+welcomeSock.listen(1)
+
+# Accept connections
+clientSock, addr = welcomeSock.accept()
+print("Accepted connection from client: ", addr)
+
+PUT_CMD = 'put'
+GET_CMD = 'get'
+QUIT_CMD = 'quit'
+LIST_CMD = 'ls'
+FORMAT = 'utf-8'
 
 
 # ************************************************
@@ -34,76 +42,147 @@ welcomeSock.listen()
 # @param numBytes - the number of bytes to receive
 # @return - the bytes received
 # *************************************************
+
 def recvAll(sock, numBytes):
+    # The buffer
+    recvBuff = ""
 
-	# The buffer
-	recvBuff = ""
-	
-	# The temporary buffer
-	tmpBuff = ""
-	
-	# Keep receiving till all is received
-	while len(recvBuff) < numBytes:
+    # The temporary buffer
+    tmpBuff = ""
 
-		# Attempt to receive bytes
-		tmpBuff = sock.recv(numBytes).decode('utf-8')
+    # Keep receiving till all is received
+    while len(recvBuff) < numBytes:
 
-		# The other side has closed the socket
-		if not tmpBuff:
-			break
-		
-		# Add the received bytes to the buffer
-		recvBuff += tmpBuff
+        # Attempt to receive bytes
+        tmpBuff = sock.recv(numBytes).decode(FORMAT)
 
-	sock.send("File received".encode('utf-8'))
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
 
-	return recvBuff
-		
-connected = True
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff
+
+    return recvBuff
+
 
 # Accept connections forever
-while connected:
+while True:
 
-	print("Waiting for connections...")
+    print("Waiting for connections...")
 
-	# Accept connections
-	clientSock, addr = welcomeSock.accept()
+    client_input = clientSock.recv(1024).decode(FORMAT)
+    print("Received client input: ", client_input)
 
-	print("Accepted connection from client: ", addr)
+    if client_input.find(PUT_CMD) != -1:
 
-	# The buffer to all data received from the
-	# the client.
-	fileData = ""
+        # The buffer to all data received from the
+        # the client.
+        fileData = ""
 
-	# The temporary buffer to store the received
-	# data.
-	recvBuff = ""
+        # The temporary buffer to store the received
+        # data.
+        recvBuff = ""
 
-	# The size of the incoming file
-	fileSize = 0
+        # The size of the incoming file
+        fileSize = 0
 
-	# The buffer containing the file size
-	fileSizeBuff = ""
+        # The buffer containing the file size
+        fileSizeBuff = ""
 
-	# Receive the first 10 bytes indicating the
-	# size of the file
-	fileSizeBuff = recvAll(clientSock, 10)
+        # Receive the first 10 bytes indicating the
+        # size of the file
+        fileSizeBuff = recvAll(clientSock, 10)
 
-	if fileSizeBuff:
+        if fileSizeBuff:
+            # Get the file size
+            fileSize = int(fileSizeBuff)
 
-		# Get the file size
-		fileSize = int(fileSizeBuff)
+            print("The file size is: ", fileSize)
 
-		print("The file size is: ", fileSize)
+            # Get the file data
+            fileData = recvAll(clientSock, fileSize)
 
-		# Get the file data
-		fileData = recvAll(clientSock, fileSize)
+            print("The file data is:\n", '"', fileData, '"\n')
 
-		print("The file data is:\n", '"', fileData, '"\n')
+    elif client_input.find(GET_CMD) != -1:
 
-		if fileData == DISCONNECT_MESSAGE:
-			print("Disconnecting connections...")
-			connected = False
+        # The name of the file
+        fileName = client_input.replace(GET_CMD, '').strip()
+        print("--> fileName: ", fileName)
 
-	# Close our side
-	clientSock.close()
+        # clientSock.send(PUT_CMD.encode(FORMAT))
+
+        # Open the file
+        fileObj = open(fileName, "r")
+
+        # Read 65536 bytes of data
+        fileData = fileObj.read(65536)
+
+        # Make sure we did not hit EOF
+        if fileData:
+
+            # Get the size of the data read
+            # and convert it to string
+            dataSizeStr = str(len(fileData))
+
+            # Prepend 0's to the size string
+            # until the size is 10 bytes
+            while len(dataSizeStr) < 10:
+                dataSizeStr = "0" + dataSizeStr
+
+            # Prepend the size of the data to the
+            # file data.
+            fileData = dataSizeStr + fileData
+
+            # The number of bytes sent
+            numSent = 0
+
+            # Send the data!
+            while len(fileData) > numSent:
+                numSent += clientSock.send(fileData[numSent:].encode(FORMAT))
+
+        # The file has been read. We are done
+        else:
+            break
+
+        print("Server done sending file")
+        fileObj.close()
+        print("Sent ", numSent, " bytes.")
+
+    elif client_input.find(LIST_CMD) != -1:
+
+        for line in subprocess.getstatusoutput('ls'):
+            print("lists files on the server", line)
+
+        # Get the size of the data read
+        # and convert it to string
+        dataSizeStr = str(len(str(line)))
+
+        # Prepend 0's to the size string
+        # until the size is 10 bytes
+        while len(dataSizeStr) < 10:
+            dataSizeStr = "0" + dataSizeStr
+
+        # Prepend the size of the data to the
+        # file data.
+        fileData = dataSizeStr + line
+
+        # The number of bytes sent
+        numSent = 0
+
+        # Send the data!
+        while len(fileData) > numSent:
+            numSent += clientSock.send(fileData[numSent:].encode(FORMAT))
+
+    elif client_input.find(QUIT_CMD) != -1:
+
+        break
+
+    else:
+
+        break
+
+# Close our side
+print("Disconnecting connections from Server")
+clientSock.close()
